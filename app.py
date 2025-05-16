c320cf0d1eb8f1ca4f8c08629bd61375a5468015
Moezhie <muziburrahman.st@gmail.com>
2025-05-17 06:06:24 +0700

    Memastikan app.py ada di root proyek

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ def home():
 
 if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5000)
+
+@app.route("/data")
+def data():
+    return {"message": "API berjalan dengan sukses!", "status": 200}
